8fd34984448f8654b615715b0c1af1656e2c29ed
Giulio Iotti <dullgiulio@gmail.com>
2015-07-16 12:10:52 +0000

    Support files with other statements.

--- osat.go
+++ osat.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -9,44 +10,20 @@ import (
 	"strings"
 )
 
-type statement struct {
-	data []byte
-}
-
-func (s statement) split(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	if s.data == nil {
-		s.data = make([]byte, len(data))
-		copy(s.data, data)
-	} else {
-		s.data = append(s.data, data...)
-	}
-	if atEOF {
-		return len(s.data), s.data, io.EOF
-	}
-	advance = strings.Index(string(s.data), ";\n")
-	if advance >= 0 {
-		advance += 2 // Include ;\n
-		token = make([]byte, advance)
-		copy(token, s.data[:advance-1]) // Next token
-		copy(s.data, s.data[advance:])  // Remaining data
-		return advance, token, nil
-	}
-	return 0, nil, nil
-}
-
-func (s statement) readAll(r io.Reader) []string {
-	lines := make([]string, 0)
+// emitLines emits every line of the file on the channel
+func emitLines(r io.Reader, c chan<- string) {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(s.split)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		c <- scanner.Text()
 	}
-	sort.Strings(lines)
-	return lines
+	close(c)
 }
 
-func mergeLines(lines []string) []string {
-	lastTable := ""
+// merge sorts the lines (each line one statement); then groups all statements by
+// table they modify.  Returns the group of statements as one single multiline string.
+func merge(lines []string) string {
+	var lastTable string
+	sort.Strings(lines)
 	for i := 0; i < len(lines); i++ {
 		if !strings.HasPrefix(lines[i], "ALTER TABLE ") ||
 			!strings.HasSuffix(lines[i], ";") {
@@ -64,14 +41,61 @@ func mergeLines(lines []string) []string {
 		}
 		lastTable = table
 	}
-	return lines
+	return strings.Join(lines, "\n")
+}
+
+// transform reads, merges and outputs to the out channel an group of consecutive ALTER statements
+func transform(out chan<- string, in <-chan string) {
+	lines := make([]string, 1)
+	lines[0] = line
+	for line := range in {
+		// End of the alter table statements: merge the found ones
+		// and remember to print out this normal line
+		if !strings.HasPrefix(line, "ALTER TABLE ") {
+			out <- merge(lines)
+			out <- line
+			return
+		}
+		// Read up until the end of the statement in one line
+		if !strings.HasSuffix(line, ";") {
+			var buf bytes.Buffer
+			buf.WriteString(line)
+			// Read until the end of this query
+			for l := range in {
+				buf.WriteString(l)
+				if strings.HasSuffix(l, ";") {
+					break
+				}
+			}
+			line = buf.String()
+		}
+		lines = append(lines, line)
+	}
+	out <- merge(lines)
+}
+
+// parse reads lines from the in channel, outputs transformed or verbatim lines to the out channel.
+func parse(out chan<- string, in <-chan string) {
+	for line := range in {
+		// Normal statement, just print it out
+		if !strings.HasPrefix(line, "ALTER TABLE ") {
+			out <- line
+			continue
+		}
+		// Read one or more ALTER TABLE statements
+		transform(out, in)
+	}
+	close(out)
 }
 
 func main() {
-	stmt := &statement{}
-	lines := mergeLines(stmt.readAll(os.Stdin))
+	in := make(chan string)
+	out := make(chan string)
 
-	for i := range lines {
-		fmt.Printf("%s\n", lines[i])
+	go emitLines(os.Stdin, in)
+	go parse(out, in)
+
+	for line := range out {
+		fmt.Printf("%s\n", line)
 	}
 }
